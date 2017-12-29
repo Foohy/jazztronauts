95df018fdc477afd30664e3064d730b5a4a0998e
Scott Kauker <swkauker@yahoo.com>
2017-12-29 03:08:53 -0800

    Added concommand "jazz_rollmap" to let anyone initiate a map roll. Debugging for now until we settle on a better system.

--- gamemodes/jazztronauts/gamemode/map/init.lua
+++ gamemodes/jazztronauts/gamemode/map/init.lua
@@ -5,3 +5,9 @@ include("mapgen.lua")
 AddCSLuaFile( "cl_init.lua" )
 AddCSLuaFile( "mapgen.lua" )
 AddCSLuaFile( "mapcontrol.lua" )
+
+concommand.Add("jazz_rollmap", function(ply, cmd, args, argstr)
+    if mapcontrol.IsInHub() then
+        mapcontrol.RollMap()
+    end
+end )

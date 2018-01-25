375a66fad2fa097d293c723ecf3da75ac31a0234
Scott Kauker <swkauker@yahoo.com>
2018-01-24 23:19:47 -0800

    Cat simple use type and is very excited to exist

--- gamemodes/jazztronauts/entities/entities/jazz_cat/shared.lua
+++ gamemodes/jazztronauts/entities/entities/jazz_cat/shared.lua
@@ -6,6 +6,7 @@ ENT.RenderGroup = RENDERGROUP_OPAQUE
 
 ENT.Model = "models/krio/jazzcat1.mdl"
 ENT.NPCID = 0 -- TODO
+ENT.AutomaticFrameAdvance = true
 
 function ENT:Initialize()
 
@@ -13,7 +14,13 @@ function ENT:Initialize()
         self:SetModel(self.Model)
         local mins, maxs = self:GetModelBounds()
         self:PhysicsInitBox(mins, maxs)
+        self:SetCollisionBounds(mins, maxs)
         self:SetMoveType(MOVETYPE_NONE)
+
+        self:SetUseType(SIMPLE_USE)
+
+        self:ResetSequence(self:LookupSequence("jig2"))
+        self:SetPlaybackRate(1.0)
     end
 
     if CLIENT then
@@ -21,6 +28,9 @@ function ENT:Initialize()
     end
 end
 
+function ENT:Think()
+
+end
 
 function ENT:Use(activator, caller)
     if IsValid(caller) and caller:IsPlayer() then 
@@ -31,6 +41,7 @@ function ENT:Use(activator, caller)
 end
 
 if SERVER then return end
+
 
 function ENT:Draw()
     self:DrawModel()

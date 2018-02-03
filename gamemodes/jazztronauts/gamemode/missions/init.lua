1605b3250f7f0bbbd89d597b4d37ae3dffaf96e4
Scott Kauker <swkauker@yahoo.com>
2018-02-02 20:15:33 -0800

    Added some utility functions for missions so we don't go totally insane jazz_missions_add (increment 1 prop for all active missions) jazz_missions_reset_all (reset missions for everybody)

--- gamemodes/jazztronauts/gamemode/missions/init.lua
+++ gamemodes/jazztronauts/gamemode/missions/init.lua
@@ -3,3 +3,24 @@ include("missions.lua")
 include("converse.lua")
 
 AddCSLuaFile("missions.lua")
+
+-- Utility mission functions so we don't go insane
+-- Remove these
+concommand.Add("jazz_missions_add", function(ply, cmd, args, argstr)
+    local m = missions.GetActiveMissions(ply)
+    for k, v in pairs(m) do
+        missions.AddMissionProgress(ply, k)
+    end
+end )
+
+concommand.Add("jazz_missions_reset_all", function(ply, cmd, args, argstr)
+    if IsValid(ply) and ply:IsAdmin() then
+        jsql.Reset("jazz_active_missions")
+
+        for _, v in pairs(player.GetAll()) do
+            missions.UpdatePlayerMissionInfo(v)
+        end
+    end
+end )
+
+

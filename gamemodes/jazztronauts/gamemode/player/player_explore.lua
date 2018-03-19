6d27a2475ec3e1352d325df7f9eb1d851c95bbd8
Scott Kauker <swkauker@yahoo.com>
2018-03-18 18:11:07 -0700

    Renamed stan, fixed a few more prediction errors. Skull spinning is much smoother now

--- gamemodes/jazztronauts/gamemode/player/player_explore.lua
+++ gamemodes/jazztronauts/gamemode/player/player_explore.lua
@@ -62,7 +62,7 @@ function PLAYER:Loadout()
 	self.Player:Give( "weapon_luaphysgun" )
 	self.Player:Give( "weapon_buscaller" )
 	self.Player:Give( "weapon_propsnatcher" )
-	self.Player:Give( "weapon_gun" )
+	self.Player:Give( "weapon_stan" )
 
     self.Player:SwitchToDefaultWeapon()
 end

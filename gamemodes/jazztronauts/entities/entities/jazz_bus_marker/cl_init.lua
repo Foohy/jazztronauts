9c68e253f57908a9e24025c318f8ac31847cde0b
Scott Kauker <swkauker@yahoo.com>
2018-04-02 22:05:30 -0700

    Bus caller automatically gives/switching to gun if you click within the summoning circle (with any weapon!!) Also spruced up the visuals a bit to make it more noticable (and run faster)

--- gamemodes/jazztronauts/entities/entities/jazz_bus_marker/cl_init.lua
+++ gamemodes/jazztronauts/entities/entities/jazz_bus_marker/cl_init.lua
@@ -1,11 +1,32 @@
 include("shared.lua")
 
+local function CopyMaterial(mat, name)
+    local vals = mat:GetKeyValues()
+    for k, v in pairs(vals) do if type(v) == "ITexture" then vals[k] = v:GetName() end end
+    vals["$translucent"] = 1 -- Why isn't this copied??
+    return CreateMaterial(name, mat:GetShader(), vals)
+end
+
+local ReticleCircleMaterial 	= CopyMaterial(Material("ui/jazztronauts/circle"), "BusMarkerMaterial" .. FrameNumber())
+local ReticleCenterMaterial     = Material("icon16/car.png")
+
+ENT.SpawnScale = 0
+function ENT:Initialize()
+
+end
 
 
 function ENT:Think()
 
-    LocalPlayer().ActiveBusMarkers = LocalPlayer().ActiveBusMarkers or {}
-    table.insert(LocalPlayer().ActiveBusMarkers, self)
+    if not self:GetIsBeingDeleted() then
+        LocalPlayer().ActiveBusMarkers = LocalPlayer().ActiveBusMarkers or {}
+        table.insert(LocalPlayer().ActiveBusMarkers, self)
+    end
+
+    -- Approach spawn scale for a nice womp in
+    local goalScale = self.GetIsBeingDeleted and self:GetIsBeingDeleted() and 0 or 1
+    self.SpawnScale = math.Approach(self.SpawnScale, goalScale, FrameTime() * 5)
+    self:SetModelScale(self.SpawnScale)
 
 end
 
@@ -16,8 +37,8 @@ end
 local function drawSemiCircle(cx, cy, w, h, perc)
     local verts = {{x = cx, y = cy}}
     
-    for i=0,127 do
-        local rad = perc * 2 * math.pi * i / 127.0
+    for i=0,32 do
+        local rad = perc * 2 * math.pi * i / 32.0
         table.insert(verts, {
             x = math.cos(rad) * w + cx,
             y = math.sin(rad) * h + cy
@@ -27,14 +48,26 @@ local function drawSemiCircle(cx, cy, w, h, perc)
     surface.DrawPoly(verts)
 end
 
+local function isMarkerLocallyHeld(marker)
+    local wep = LocalPlayer():GetWeapon("weapon_buscaller")
+    if not IsValid(wep) or wep != LocalPlayer():GetActiveWeapon() then return false end
+
+    return marker == wep:GetBusMarker()
+end
+
 hook.Add( "PostDrawHUD", "JazzDrawBusMarker", function()
     local markers = LocalPlayer().ActiveBusMarkers
     if !markers or #markers == 0 then return end
-    
-    //PrintTable(Vector():ToScreen())
+ 
 	cam.Start2D()
+        local pfov = LocalPlayer():GetFOV()
+
         for _, v in pairs(markers) do
             if !IsValid(v) then continue end
+            local isLook = v:IsLookingAt(EyePos(), EyeAngles():Forward(), LocalPlayer():GetFOV()) or isMarkerLocallyHeld(v)
+            local isMoving = v:GetSpawnPercent() > 0
+            v.SmoothPercent = v.SmoothPercent or 0     
+            v.SmoothPercent = math.Approach(v.SmoothPercent, v:GetSpawnPercent(), FrameTime() * 0.25)
 
             -- ToScreen only works in a 3d rendering context....
             local scrpos = nil
@@ -43,11 +76,36 @@ hook.Add( "PostDrawHUD", "JazzDrawBusMarker", function()
             local x = math.Clamp(scrpos.x, 100, ScrW() - 100)
             local y = math.Clamp(scrpos.y, 100, ScrH() - 100)
 
-            local circSize = ScreenScale(20)
+	        local radius = (ScrW() / 2) * math.tan(math.rad(90 - pfov/2)) * math.tan(math.rad(v.CircleCone))
             surface.SetDrawColor( 255, 0, 0, 255 )
 	        draw.NoTexture()
-            surface.DrawCircle(x, y, circSize, 255, 255, 255, 100)
-            drawSemiCircle(x, y, circSize, circSize, v:GetSpawnPercent())
+            //surface.DrawCircle(x, y, radius, 255, isLook and 0 or 255, 255, 100)
+
+            draw.NoTexture()
+            surface.SetDrawColor(255, 255, 255, 100)
+            drawSemiCircle(x, y, radius * 0.73, radius* 0.73, v:GetSpawnPercent())
+            
+            local size = radius * 2.55
+            ReticleCircleMaterial:SetFloat("$glowstart", isLook and 0 or 1)
+            ReticleCircleMaterial:SetFloat("$glowend", 1.0)
+            ReticleCircleMaterial:SetFloat("$glowalpha", 1)
+
+            ReticleCircleMaterial:SetFloat("$edgesoftnessstart", .48)
+            ReticleCircleMaterial:SetFloat("$edgesoftnessend", 0.4 - v:GetSpawnPercent() * 0.4)
+
+            ReticleCircleMaterial:SetVector("$glowcolor", Vector(255/255.0, 247/255.0, 114/255.0))
+            surface.SetMaterial(ReticleCircleMaterial)
+            surface.SetDrawColor(isLook and 30 or 255, 255, isLook and 30 or 255, 255)
+	        surface.DrawTexturedRect(x - size/2, y - size/2, size, size)
+
+            local size = radius * 1
+            surface.SetMaterial(ReticleCenterMaterial)
+            surface.SetDrawColor(255, 255, 255, 255)
+            local rot = math.sin(CurTime()* 4) * 20
+            rot = rot + ( v.SmoothPercent + math.pow(v.SmoothPercent * 100, 2))
+
+            surface.DrawTexturedRectRotated(x, y, size, size, rot)
+
 
             //draw.DrawText(tostring(v:GetSpawnPercent()), nil, x, y)
         end
--- gamemodes/jazztronauts/entities/entities/jazz_bus_marker/shared.lua
+++ gamemodes/jazztronauts/entities/entities/jazz_bus_marker/shared.lua
@@ -2,16 +2,30 @@
 ENT.Type = "anim"
 ENT.Base = "base_anim"
 ENT.RenderGroup = RENDERGROUP_OPAQUE
-ENT.Model       = "models/Combine_Helicopter/helicopter_bomb01.mdl"
+ENT.Model       = "models/props_trainstation/trainstation_clock001.mdl"
 
 ENT.SpawnDelay = 3
+ENT.CircleCone = 3
 
 function ENT:SetupDataTables()
     self:NetworkVar("Float", 0, "SpawnTime")
+    self:NetworkVar("Bool", 0, "IsBeingDeleted")
 end
 
 function ENT:CountdownStarted()
     return self:GetSpawnTime() != 0 
+end
+
+function ENT:IsLookingAt(pos, eyedir, pfov)
+    if self:GetIsBeingDeleted() then return false end
+    
+	pfov = pfov or 90
+	local ang = math.cos(math.rad(self.CircleCone))
+
+    local dir = (self:GetPos() - pos)
+	dir:Normalize()
+
+	return eyedir:Dot(dir) >= ang 
 end
 
 function ENT:GetSpawnPercent()

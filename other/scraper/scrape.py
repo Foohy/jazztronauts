033b0f01486efde4866af4fc545eaaeea527a96d
Scott Kauker <swkauker@yahoo.com>
2018-04-08 04:41:43 -0700

    Updated scraper, ignores addons with 'server' and 'content' in the title.

--- other/scraper/scrape.py
+++ other/scraper/scrape.py
@@ -2,13 +2,26 @@ import sys
 import json
 import time
 import urllib2
+import re
 
 HOST = "http://api.steampowered.com"
 ENDPOINT = "IPublishedFileService/QueryFiles/v0001/"
 APPID = 4000
 NUMPERPAGE = 100
-DELAY = 0.0 # How long to delay between requests
+DELAY = 0.1 # How long to delay between requests
 FILENAME = "addons.txt"
+
+ignore_words = ["content", "server"]
+ignore_reg = "(?<!_){0}(?!_)" # Allow ignore words to be a part of the map name (surrounding underscores)
+def containsIgnoreWord(str, word):
+    return re.search(ignore_reg.format(word), str) is not None
+
+def containsIgnoreWords(str):
+    for word in ignore_words:
+        if containsIgnoreWord(str, word):
+            return True
+        
+    return False
 
 if __name__ == "__main__":
 
@@ -16,33 +29,41 @@ if __name__ == "__main__":
         print("A Steam WebAPI key is required.")
         sys.exit(1)
 
-    f = open(FILENAME, "w")
-
     key = sys.argv[1]
     page = 0
+    workshopids = []
 
     while True:
-        req = "{0}/{1}?key={2}&appid={3}&requiredtags[0]=map&numperpage={4}&page={5}".format(HOST, ENDPOINT, key, APPID, NUMPERPAGE, page)
+        req = "{0}/{1}?key={2}&appid={3}&requiredtags[0]=map&numperpage={4}&page={5}&return_metadata=1".format(HOST, ENDPOINT, key, APPID, NUMPERPAGE, page)
         response = urllib2.urlopen(req).read()
-        resobj = json.loads(response)
+        resobj = json.loads(response.decode("utf-8", "ignore"))
         total = resobj["response"]["total"]
-        # print(len(resobj["response"]["publishedfiledetails"]))
+
         for addon in resobj["response"]["publishedfiledetails"]:
-            f.write(addon["publishedfileid"] + "\n")
+            if "title" in addon and containsIgnoreWords(addon["title"]):
+                print("Ignoring: " + addon["title"])
+                continue
+
+            workshopids.append(addon["publishedfileid"])
 
         # Informative output
         finished = page * NUMPERPAGE + len(resobj["response"]["publishedfiledetails"])
-        print("Finished {0} addons. ({1:.2f}% of {2})".format(finished, finished * 100.0 / total, total))
-
-        # so valve doesn't get angry at us
-        time.sleep(0.1)
+        #print("Finished {0} addons. ({1:.2f}% of {2})".format(finished, finished * 100.0 / total, total))
 
         # Move onto to the next page
         page += 1
 
         if page * NUMPERPAGE > resobj["response"]["total"]:
             break
+        else:   
+            # so valve doesn't get angry at us
+            time.sleep(DELAY)
     
+    print("Dumping {0} addons to {1}".format(len(workshopids), FILENAME))
+    f = open(FILENAME, "w")
+    for id in workshopids:
+        f.write(id + "\n")
+
     print("Finished!!")
     f.close()
 

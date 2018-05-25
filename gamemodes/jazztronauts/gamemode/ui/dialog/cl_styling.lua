7fe3c977713583978850f3b90e4c833062f4216e
Scott Kauker <swkauker@yahoo.com>
2018-05-24 18:54:54 -0700

    Dialog now has basic styling and cat cut-in

--- gamemodes/jazztronauts/gamemode/ui/dialog/cl_styling.lua
+++ gamemodes/jazztronauts/gamemode/ui/dialog/cl_styling.lua
@@ -1,10 +1,50 @@
 -- This file controls how the dialog should be drawn/styled/progressed
 -- Handles text rendering, option selection, and scene selection.
 
-surface.CreateFont( "DialogFont", {
-	font = "Arial", -- Use the font-name which is shown to you by your operating system Font Viewer, not the file name
+local chatboxMat = Material("materials/ui/chatbox.png", "alphatest")
+
+-- Position of top left corner of text, relative to dialog background
+local TextX = ScreenScale(80)
+local TextY = ScreenScale(25)
+
+-- Position of top left corner for name text
+local NameTextX = ScreenScale(75)
+local NameTextY = ScreenScale(20)
+
+-- Position of top left corner of dialog background
+local BGOffX = ScreenScale(60)
+local BGOffY = ScreenScale(10)
+
+local BGW = ScreenScale(500)
+local BGH = ScreenScale(90)
+
+
+local CatW = ScreenScale(150)
+local CatH = ScreenScale(170)
+local CatCamOffset = Vector(-60, -35, 0):GetNormal() * 70
+
+surface.CreateFont( "JazzDialogNameFont", {
+	font = "KG Shake it Off Chunky",
 	extended = false,
-	size = 25,
+	size = ScreenScale(15),
+	weight = 500,
+	blursize = 0,
+	scanlines = 0,
+	antialias = true,
+	underline = false,
+	italic = false,
+	strikeout = false,
+	symbol = false,
+	rotary = false,
+	shadow = false,
+	additive = false,
+	outline = false,
+} )
+
+surface.CreateFont( "JazzDialogFont", {
+	font = "Arial",
+	extended = false,
+	size = ScreenScale(15),
 	weight = 500,
 	blursize = 0,
 	scanlines = 0,
@@ -43,33 +83,67 @@ local function CreateResponseButtons(data)
 	frame:SizeToChildren(true, true)
 end
 
+local function RenderCatCutIn(_dialog, x, y, w, h)
+	local cat = dialog.GetFocus()
+	if not IsValid(cat) then return end
+	local headpos = cat:GetPos() + cat:GetAngles():Up() * 49
+
+	local pos = headpos + cat:GetAngles():Forward() * CatCamOffset.X + cat:GetAngles():Right() * CatCamOffset.Y
+	local ang = (headpos - pos):Angle()
+
+	cam.Start3D(pos, ang, 25, x, y, w, h)
+		cat:DrawModel()
+	cam.End3D()
+end
+
+local function GetCurrentSpeaker()
+	local speaker = dialog.GetFocus()
+	if not IsValid(speaker) || !speaker.GetNPCID then return "" end
+
+	return string.upper(missions.GetNPCName(speaker:GetNPCID()))
+end
+
 local function RenderDialog(_dialog)
-    local width, height = 850, 200
 	if _dialog.open == 0 then return end
 
 	local open = math.sin( _dialog.open * math.pi / 2 )
 	open = math.sqrt(open)
 
-	local w = width * open
-	local h = height * open
+	local w = BGW * open
+	local h = BGH * open
 
-	local x = ScrW() / 2
-	local y = ScrH() - h
+	local x = ScrW() / 2 + BGOffX
+	local y = ScrH() - h/2 - BGOffY
 
-	surface.SetDrawColor( 0,0,0,190 )
-	surface.DrawRect( x - w/2, y - h/2, w, h )
+	surface.SetMaterial(chatboxMat)
+	surface.SetDrawColor( 255, 255, 255, 255 )
+	surface.DrawTexturedRectUV( x - w/2, y - h/2, w, h, 0, 0, 1, 1)
 
-	local left = x - 800/2 // - _dialog.textw / 2
-	local top = y - 200/2//- _dialog.texth / 2
+	local left = x - w/2 + NameTextX
+	local top = y - h/2 + NameTextY
 
-	surface.SetTextColor( 255, 255, 255, 255 * open )
-	surface.SetFont( "DialogFont" )
+	surface.SetTextColor( 64, 38, 49, 255 * open )
+	surface.SetFont( "JazzDialogNameFont" )
     local tw,th = surface.GetTextSize( "a" )
+
+	-- Draw current speaker's name
+	surface.SetTextPos( left, top - th)
+	surface.DrawText( GetCurrentSpeaker())
+
+	surface.SetFont( "JazzDialogFont" )
+    local tw,th = surface.GetTextSize( "a" )
+	left = x - w/2 + TextX
+	top = y - h/2 + TextY
+
+	-- Draw dialog contents
 	local lines = string.Explode( "\n", _dialog.printed )
 	for k, line in pairs(lines) do
 		surface.SetTextPos( left, top + th * (k-1) )
 		surface.DrawText( line )
 	end
+
+	-- Render whoever's talking
+	RenderCatCutIn(_dialog, 0, ScrH() - CatH * math.EaseInOut(_dialog.open, 0, 1), CatW, CatH)
 end
 
 

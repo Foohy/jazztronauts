131b6117b92f0407eb69bb65363bf506c22d4f90
Scott Kauker <swkauker@yahoo.com>
2018-06-30 17:02:24 -0700

    Finally just added a global hook, "OnClientInitialized" to be called when the client does one tick and tells the server

---
+++ gamemodes/jazztronauts/gamemode/lib/sh_playerinit.lua
@@ -0,0 +1,20 @@
+-- Just implementing the good ol hook for when a player's client is init'd and ready
+AddCSLuaFile()
+
+if SERVER then
+    util.AddNetworkString("JazzPlayerClientStartup")
+
+    net.Receive("jazzPlayerClientStartup", function(len, ply)
+        if ply.JazzHasClientStarted then return end
+
+        ply.JazzHasClientStarted = true
+        hook.Run("OnClientInitialized", ply)
+    end )
+else
+    hook.Add("Think", "JazzPlayerClientStartup", function()
+        hook.Remove("Think", "JazzPlayerClientStartup")
+
+        net.Start("JazzPlayerClientStartup")
+        net.SendToServer()
+    end )
+end

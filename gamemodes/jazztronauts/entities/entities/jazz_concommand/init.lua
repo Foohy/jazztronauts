d53a3604505dee4d8ccfac1ccd2040173d44cca9
Scott Kauker <swkauker@yahoo.com>
2018-07-12 13:53:53 -0700

    added jazz_concommand, imitating point_servercommand WITHOUT the risk of corrupting your vmf

---
+++ gamemodes/jazztronauts/entities/entities/jazz_concommand/init.lua
@@ -0,0 +1,30 @@
+-- Runs concommands without the risk of currupting the vmf
+ENT.Type = "point"
+ENT.DisableDuplicator = true
+
+function ENT:Initialize()
+
+	self.script = ""
+
+end
+
+function ENT:KeyValue( key, value )
+
+	if key == "Concommand" then self.Concommand = value end
+
+end
+
+
+
+function ENT:RunCommand( activator, caller, data )
+
+	RunConsoleCommand(self.Concommand)
+
+end
+
+function ENT:AcceptInput( name, activator, caller, data )
+
+	if name == "RunCommand" then self:RunCommand( activator, caller, data ) return true end
+
+	return false
+end

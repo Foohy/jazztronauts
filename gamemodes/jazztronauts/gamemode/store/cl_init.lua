5aea3d0de4810b6656f55a803fc9caa73b088207
Scott Kauker <swkauker@yahoo.com>
2018-07-30 03:05:06 -0700

    Added optional third party weapons ( if they're installed)

--- gamemodes/jazztronauts/gamemode/store/cl_init.lua
+++ gamemodes/jazztronauts/gamemode/store/cl_init.lua
@@ -1,1 +1,2 @@
 include("jstore.lua")
+include("thirdparty.lua")
--- gamemodes/jazztronauts/gamemode/store/init.lua
+++ gamemodes/jazztronauts/gamemode/store/init.lua
@@ -1,3 +1,5 @@
 include("jstore.lua")
+include("thirdparty.lua")
 AddCSLuaFile("cl_init.lua")
 AddCSLuaFile("jstore.lua")
+AddCSLuaFile("thirdparty.lua")

911e5f1cffe1e9ceb030036226e753a43a1ec051
Scott Kauker <swkauker@yahoo.com>
2018-07-31 13:11:35 -0700

    New naughty altar model + lit up candles

--- gamemodes/jazztronauts/entities/entities/jazz_door_eclipse/init.lua
+++ gamemodes/jazztronauts/entities/entities/jazz_door_eclipse/init.lua
@@ -7,8 +7,13 @@ AddCSLuaFile("cl_init.lua")
 ENT.DoorOpen = Sound("doors/door1_move.wav") //just defaults
 ENT.DoorClose = Sound("doors/door_wood_close1.wav") //just defaults
 
+ENT.CandleModel = Model("models/sunabouzu/gameplay_candle.mdl")
+ENT.CandleRadiusX = 75
+ENT.CandleRadiusY = 50
+PrecacheParticleSystem("jazzCandle")
+
 function ENT:Initialize()
-	self:SetModel("models/sunabouzu/jazzdoor.mdl")
+	self:SetModel("models/props_c17/gravestone_coffinpiece002a.mdl")
 	self:SetMoveType(MOVETYPE_NONE)
 	self:SetSolid(SOLID_VPHYSICS)
 	self:SetUseType(SIMPLE_USE)	
@@ -30,8 +35,9 @@ function ENT:Initialize()
 	-- If we have somewhere to go, spawn the voter machine
 	if self:GetDestination() then
 		local votium = ents.Create("jazz_vote_podiums")
-		votium:SetPos(self:GetPos())
+		votium:SetPos(self:GetPos() + self:GetAngles():Up() * -10)
 		votium:SetAngles(self:GetAngles())
+		votium:SetKeyValue("PodiumRadius", 85)
 		votium:Spawn()
 		votium:Activate()
 		votium:SetParent(self)
@@ -40,7 +46,34 @@ function ENT:Initialize()
 		end )
 
 		self.VotePodium = votium
+
+		self:SpawnShardCount()
 	end
+end
+
+function ENT:SpawnShardCount()
+	local shardcount = mapgen.GetTotalCollectedBlackShards()
+	local required = mapgen.GetTotalRequiredBlackShards()
+	if shardcount == 0 then return end
+
+	for i=1, required do
+		local p = i * 1.0 / required
+		local ang = 2 * math.pi * p
+		local candle = ents.Create("prop_dynamic")
+		candle:SetModel(self.CandleModel)
+		candle:SetPos(self:GetPos() + Vector(math.cos(ang) * self.CandleRadiusX, math.sin(ang) * self.CandleRadiusY, -9))
+		candle:Spawn()
+		candle:Activate()
+
+		if shardcount > 0 then
+			shardcount = shardcount - 1
+			local ed = EffectData()
+			ed:SetOrigin(candle:GetPos())
+			ed:SetEntity(candle)
+			ParticleEffect("jazzCandle", candle:GetPos() + Vector(0, 0, 12), candle:GetAngles(), candle )
+		end	
+	end
+
 end
 
 function ENT:OnChangeDestination()
@@ -49,34 +82,17 @@ function ENT:OnChangeDestination()
 	if dest == self.DEST_ENCOUNTER then
 		newgame.SetGlobal("unlocked_encounter", true)
 		mapcontrol.Launch(mapcontrol.GetEncounterMap())
+	end/*
 	elseif dest == self.DEST_ENDGAME then 
 		newgame.SetGlobal("ending", newgame.ENDING_ECLIPSE)
 		mapcontrol.Launch(mapcontrol.GetEndMaps()[newgame.ENDING_ECLIPSE])
 	end
+	*/
 
 end 
 
 function ENT:Use(activator, caller)
 	self:TriggerOutput("OnUse", activator)
-
-	if IsValid(activator) && !activator.Teleporting then
-		--self:StartLoading( activator )
-
-		local sequence = self:LookupSequence("open")
-
-		if (self:GetSequence() != sequence ) then
-			self:ResetSequence(sequence)
-			self:SetPlaybackRate(1.0)
-
-			local door = self:GetLinkedDoor()
-			if IsValid( door ) then
-				door:ResetSequence( sequence )
-				door:SetPlaybackRate( 1.0 )
-			end
-
-			self:EmitSound( self.DoorOpen )
-		end
-	end
 end
 
 

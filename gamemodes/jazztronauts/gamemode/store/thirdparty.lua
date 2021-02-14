371a46ea2bcf69fb8f3c8541043013c8f267a82e
Foohy <foohyf@gmail.com>
2021-02-13 22:32:43 -0800

    Fixed wowozela not showing up in store

--- gamemodes/jazztronauts/gamemode/store/thirdparty.lua
+++ gamemodes/jazztronauts/gamemode/store/thirdparty.lua
@@ -1,15 +1,20 @@
-local weps = weapons.GetList()
-local lookup = {}
-for _, v in pairs(weps) do
-	lookup[v.ClassName] = v
-end
-local function AddWeapon(name, price, options)
-	if not lookup[name] then return end
+timer.Simple(0, function() -- Gross, but gives all weapons a chance to register themselves first
+	local weps = weapons.GetList()
+	local lookup = {}
+	for _, v in pairs(weps) do
+		lookup[v.ClassName] = v
+	end
+	local function AddWeapon(name, price, options)
+		if not lookup[name] then 
+			print("Can't add " .. name .. " to Jazztronauts store. Not registered yet!")
+			return 
+		end
 
-	options = options or {}
-	options.thirdparty = true
-	return jstore.Register(lookup[name], price, options)
-end
+		options = options or {}
+		options.thirdparty = true
+		return jstore.Register(lookup[name], price, options)
+	end
 
--- Wowozela
-AddWeapon("wowozela", 50000, { type = "tool" })
+	-- Wowozela
+	AddWeapon("wowozela", 50000, { type = "tool" })
+end )

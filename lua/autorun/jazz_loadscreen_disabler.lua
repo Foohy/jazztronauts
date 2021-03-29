8ab5c7b798b0af52b751534eae04975dd6daf2b5
Foohy <foohyf@gmail.com>
2021-03-28 18:25:47 -0700

    Clearing the cache actually works now, and clears even when starting other gamemodes (because it has to delete on a fresh gmod startup when the files are unmounted).

--- lua/autorun/jazz_loadscreen_disabler.lua
+++ lua/autorun/jazz_loadscreen_disabler.lua
@@ -3,9 +3,21 @@
 local jazz_var = "sv_loadingurl"
 local jazz_url = "host.foohy.net/"
 
+local WORKSHOP_CACHE_PATH = "jazztronauts/cache"
+
+local function ClearCache()
+	local files = file.Find(WORKSHOP_CACHE_PATH .. "/*", "DATA")
+	for _, v in pairs(files) do
+		file.Delete(WORKSHOP_CACHE_PATH .. "/" .. v)
+	end
+end
+
 hook.Add("Initialize", "jazz_disable_loadscreen", function()
     local convar = GetConVar(jazz_var)
     if convar and string.find(convar:GetString(), jazz_url, 1, true) then
         RunConsoleCommand(jazz_var, "")
     end
+
+    -- Also clear the jazz cache of downloaded maps
+    ClearCache()
 end)

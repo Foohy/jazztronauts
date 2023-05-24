c07f90550aa89b59ea69cc42f954723ef5c7bf53
Sierra Foxtrot <sagejfox@aol.com>
2023-05-24 14:59:21 -0500

    One more for info_busstop
    Updated so the spawned trolley's travel time and entrance/exit travel distances can be set by the info_busstop spawning it.

--- gamemodes/jazztronauts/entities/entities/info_busstop/shared.lua
+++ gamemodes/jazztronauts/entities/entities/info_busstop/shared.lua
@@ -4,6 +4,9 @@ AddCSLuaFile("sh_honk.lua")
 include("sh_honk.lua")
 
 ENT.Type = "point"
+ENT.TravelTime = 2.5
+ENT.LeadUp = 2000
+ENT.TravelDist = 4500
 
 function ENT:Initialize()
 
@@ -21,10 +24,23 @@ function ENT:Initialize()
 	end
 end
 
+function ENT:KeyValue(key, value)
+	if key == "traveltime" then
+		self.TravelTime = tonumber(value)
+	elseif key == "leadup" then
+		self.LeadUp = tonumber(value)
+	elseif key == "traveldist" then
+		self.TravelDist = tonumber(value)
+	end
+end
+
 function ENT:OnMapChanged(newmap, wsid) 
 	local bus = ents.Create( "jazz_bus_hub" )
 		bus:SetPos(self:GetPos())
 		bus:SetAngles(self:GetAngles())
 		bus:SetMap(newmap, wsid or "")
+		bus.TravelTime = self.TravelTime
+		bus.LeadUp = self.LeadUp
+		bus.TravelDist = self.TravelDist
 		bus:Spawn()
 end

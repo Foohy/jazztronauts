072c99e0f03e0683e9c1d100a4cf787248f346be
Sierra Foxtrot <sagejfox@aol.com>
2023-06-03 18:07:14 -0500

    factgen tweak
    Fixes comments with commas in the Steam account name

--- gamemodes/jazztronauts/gamemode/map/factgen/sv_factgen.lua
+++ gamemodes/jazztronauts/gamemode/map/factgen/sv_factgen.lua
@@ -65,9 +65,8 @@ local function getWorkshopFacts(wsid, addFact)
 
 		local comments = task.Await(commentTask)
 		if #comments > 0 then
-			local comm = table.Random(comments)
-			comm.message = string.Replace(comm.message,",","‚") --replaces comma with U+201A "Single Low-9 Quotation Mark" (commas in comments break running through screen localization)
-			addFact("comment", "\"" .. comm.message .. "\"\n-" .. comm.author)
+			local comm = table.remove(comments,math.random(#comments))
+			addFact("comment", string.Replace("“" .. comm.message .. "”\n-" .. comm.author,",","‚"))--replaces comma with U+201A "Single Low-9 Quotation Mark" (commas in comments break running through screen localization)
 		end
 	end
 end
